e0f096a022996d04e608df525affed01151300ac
hesy <heshuying@haier.com>
2020-06-28 14:04:03 +0800

    格式化修改

--- chapter12/src/ConnectionPoolImpl1.java
+++ chapter12/src/ConnectionPoolImpl1.java
@@ -2,66 +2,71 @@ import java.sql.*;
 import java.util.*;
 
 public class ConnectionPoolImpl1 implements ConnectionPool {
-  private ConnectionProvider provider=new ConnectionProvider();
-  private final ArrayList<Connection> pool = new ArrayList<Connection>();
-  private int poolSize=5;
+    private ConnectionProvider provider = new ConnectionProvider();
+    private final ArrayList<Connection> pool = new ArrayList<Connection>();
+    private int poolSize = 5;
 
-  public ConnectionPoolImpl1(){}
-  public ConnectionPoolImpl1(int poolSize){
-    this.poolSize=poolSize;
-  }
-  
-  /** 从连接池中取出连接 */
-  public Connection getConnection() throws SQLException {
-    synchronized (pool) {
-      if ( !pool.isEmpty()){
-        int last = pool.size() - 1;
-	Connection con =pool.remove(last);
-	return con;
-      }
+    public ConnectionPoolImpl1() {
     }
-		
-    Connection con= provider.getConnection();
-    return con;
-  }
-  
-  /** 把连接返回连接池 */	
-  public void releaseConnection(Connection con) throws SQLException {
-    synchronized (pool) {
-      int currentSize = pool.size();
-      if( currentSize < poolSize ) {
-	 pool.add(con);
-         return;
-      }
-    }
-    
-    try {
-      con.close();
-    }catch (SQLException e) {e.printStackTrace();}
-  }
-	
-  protected void finalize() {
-    close();
-  }
 
-  /** 关闭连接池*/	
-  public void close() {
-    Iterator<Connection> iter = pool.iterator();
-    while ( iter.hasNext()) {
-      try {
-        iter.next().close();
-      }catch (SQLException e){e.printStackTrace();}
+    public ConnectionPoolImpl1(int poolSize) {
+        this.poolSize = poolSize;
     }
-    pool.clear();		
-  }
+
+    /**
+     * 从连接池中取出连接
+     */
+    public Connection getConnection() throws SQLException {
+        synchronized (pool) {
+            if (!pool.isEmpty()) {
+                int last = pool.size() - 1;
+                Connection con = pool.remove(last);
+                return con;
+            }
+        }
+
+        Connection con = provider.getConnection();
+        return con;
+    }
+
+    /**
+     * 把连接返回连接池
+     */
+    public void releaseConnection(Connection con) throws SQLException {
+        synchronized (pool) {
+            int currentSize = pool.size();
+            if (currentSize < poolSize) {
+                pool.add(con);
+                return;
+            }
+        }
+
+        try {
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void finalize() {
+        close();
+    }
+
+    /**
+     * 关闭连接池
+     */
+    public void close() {
+        Iterator<Connection> iter = pool.iterator();
+        while (iter.hasNext()) {
+            try {
+                iter.next().close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        pool.clear();
+    }
 }
-
-
-
-
-
-
-
 
 
 /****************************************************
--- chapter12/src/DataSourceImpl.java
+++ chapter12/src/DataSourceImpl.java
@@ -3,36 +3,44 @@ import javax.sql.DataSource;
 import java.io.*;
 import java.util.logging.*;
 
-public class DataSourceImpl implements DataSource{
-  private ConnectionPool pool=new ConnectionPoolImpl2(); 
-  public Connection getConnection()throws SQLException{return pool.getConnection();} 
-  public Connection getConnection(String username, String password)throws SQLException{
-    throw new UnsupportedOperationException();
-  } 
-  public int getLoginTimeout()throws SQLException{
-    return DriverManager.getLoginTimeout();
-  } 
-  public PrintWriter getLogWriter()throws SQLException{
-    return DriverManager.getLogWriter();
-  } 
-  public void setLoginTimeout(int seconds)throws SQLException{
-    DriverManager.setLoginTimeout(seconds);
-  } 
-  public void setLogWriter(PrintWriter out)throws SQLException{
-    DriverManager.setLogWriter(out);
-  } 
-  
-  public Logger getParentLogger()throws SQLFeatureNotSupportedException{
-    return null;
-  }
+public class DataSourceImpl implements DataSource {
+    private ConnectionPool pool = new ConnectionPoolImpl2();
 
-  public <T> T unwrap(Class<T> iface) throws SQLException {
-    return null;
-  }
- 
-  public boolean isWrapperFor(Class<?> iface) throws SQLException {
-    return false;
-  }
+    public Connection getConnection() throws SQLException {
+        return pool.getConnection();
+    }
+
+    public Connection getConnection(String username, String password) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getLoginTimeout() throws SQLException {
+        return DriverManager.getLoginTimeout();
+    }
+
+    public PrintWriter getLogWriter() throws SQLException {
+        return DriverManager.getLogWriter();
+    }
+
+    public void setLoginTimeout(int seconds) throws SQLException {
+        DriverManager.setLoginTimeout(seconds);
+    }
+
+    public void setLogWriter(PrintWriter out) throws SQLException {
+        DriverManager.setLogWriter(out);
+    }
+
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+    }
+
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return null;
+    }
+
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return false;
+    }
 }
 
 
